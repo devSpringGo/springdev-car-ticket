ba9b5d8ed475062f0271e7474f8c6b451d391041
Nguyễn Xuân Tiến <xuantien8a1@gmail.com>
2026-01-31 23:41:53 +0700

    change url

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const swaggerUi = require("swagger-ui-express"); // ⭐ THIẾU DÒNG NÀY
 const swaggerSpec = require("./config/swagger");
 //swagger end
 const app = express();
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors({ origin: "https://springdev-car-ticket.onrender.com/" }));
 app.use(express.json());
 //swagger
 app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
